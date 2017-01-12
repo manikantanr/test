f115959f010ed067668592f03c02cd46809d063f
manikantanr <manikantanr@biarca.com>
2017-01-12 12:23:29 +0530

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "from bash first line"
+
+python -c "
+import time 
+for i in range(150):
+    print 'This prints once a sec.'+str(i)
+    time.sleep(1) "
+
+echo "from bash last line"
