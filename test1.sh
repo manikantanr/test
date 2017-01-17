ac767b98224b53592803543a3554f52217766153
manikantanr <manikantanr@biarca.com>
2017-01-17 10:28:17 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -5,7 +5,7 @@ echo "from bash first line"
 python -c "
 import time 
 for i in range(80):
-    #print 'This prints once a sec.'+str(i)
+    print 'This prints once a sec.'+str(i)
     time.sleep(1) "
 
 echo "from bash last line"
